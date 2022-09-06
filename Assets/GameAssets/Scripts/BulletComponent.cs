f68acb982bb88791cb3fca01074d6bcf6d7c7c3f
AhmedSiddique <103933141+AhmedSiddiqueKevuru@users.noreply.github.com>
2022-09-06 21:08:05 +0500

    Update 2

---
+++ Assets/GameAssets/Scripts/BulletComponent.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletComponent : MonoBehaviour
+{
+    public void Start()
+    {
+        //if(TryGetComponent(out Rigidbody rb))
+        //{
+        //    rb.AddForce(Vector3.forward * 100f, ForceMode.Impulse);
+        //}
+
+
+    }
+
+}
